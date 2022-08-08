a347f36a150a7ecc90c23a31092de8f79bf67800
Casey Hudler <chudler624@gmail.com>
2022-08-08 11:40:58 -0400

    initial commit

---
+++ Methods Exercise 1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(" Hello!, What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hi {name}, How old are you?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine($" Wow, {name}, so you're {age} years old?");
